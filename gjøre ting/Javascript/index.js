8d8966b0a31bda13ae8746942b321f8e33684c44
Magnus <Lavageiren@gmail.com>
2025-11-12 14:04:26 +0100

    16.11/3

--- gjøre ting/Javascript/index.js
+++
@@ -1,5 +0,0 @@
-let username;
-document.getElementById("mySubmit").oneclick = function() {
-    username = document.getElementById("myText").value;
-    document.getElementById("myH1").textContent = `Hello ${username}`;
-}
